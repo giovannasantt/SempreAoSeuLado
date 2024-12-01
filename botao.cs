f69cffd2ab830755e245565e543f0b71deb5c8c3
Giovanna D'Santana <160945639+giovannasantt@users.noreply.github.com>
2024-12-01 17:35:31 -0300

    Update botao.cs

--- botao.cs
+++ botao.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class botao : MonoBehaviour
+public class botao : MonoBehaviour //criando o script botao, que herda do monobehaviour (executável do unity)
 {
-    public void Jogar()
+    public void Jogar() //criando o método jogar
     {
-        SceneManager.LoadScene("Jogo");
+        SceneManager.LoadScene("Jogo"); //definindo a cena que será iniciada quando o método for executado
     }
-    public void Sair()
+    public void Sair() //criando o método sair
     {
-        Application.Quit();
+        Application.Quit(); //definindo que o jogo será fechado quando o método for executado
     }
-    public void Reiniciar()
+    public void Reiniciar() //criando o método reiniciar
     {
-        SceneManager.LoadScene("Jogo");
+        SceneManager.LoadScene("Jogo"); //definindo a cena que será iniciada quando o método for executado
     }
-    public void Voltar()
+    public void Voltar() //criando o método voltar
     {
-        SceneManager.LoadScene("Inicio");
+        SceneManager.LoadScene("Inicio"); //definindo a cena que será iniciada quando o método for executado
     }
 
 
