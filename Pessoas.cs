4cec053dde742ba5113efbf080a1dd52db578398
Giovanna D'Santana <160945639+giovannasantt@users.noreply.github.com>
2024-12-01 17:24:34 -0300

    Update Pessoas.cs

--- Pessoas.cs
+++ Pessoas.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pessoas : Personagem
+public class Pessoas : Personagem //criando a classe pessoas, herdando da classe personagem
 {
-    public static int InstanciasPessoas = 0;
-    GameObject ppessoa;
-    float velocidade;
+    public static int InstanciasPessoas = 0; //contador inicial 
+    GameObject ppessoa; //chamando o gameobject ppessoa
+    float velocidade; //chamando a propriedade velocidade
   
-    public Pessoas (Vector3 posicao, GameObject pessoa)
+    public Pessoas (Vector3 posicao, GameObject pessoa) //constructor da classe pessoas
     {
-        InstanciasPessoas += 1;
-        this.Posicao = posicao;
-        this.ppessoa = pessoa;
+        InstanciasPessoas += 1; //definindo o valor do contador de pessoas
+        this.Posicao = posicao; //validando a propriedade posicao
+        this.ppessoa = pessoa; //validando o gameobject ppessoa
 
     }
     // Start is called before the first frame update
